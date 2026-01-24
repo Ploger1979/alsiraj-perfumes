bce2fe583f3ce7bdbc005f9046d2443ae93f17ef
Ayman Plöger <aymanploger@gmail.com>
2026-01-24 14:16:39 +0100

    switch email provider to hostinger

--- app/api/auth/reset-request/route.ts
+++ app/api/auth/reset-request/route.ts
@@ -41,7 +41,9 @@ export async function POST(request: Request) {
         // Attempt to send email
         if (process.env.EMAIL_USER && process.env.EMAIL_PASS) {
             const transporter = nodemailer.createTransport({
-                service: 'gmail',
+                host: 'smtp.hostinger.com',
+                port: 465,
+                secure: true, // true for 465, false for other ports
                 auth: {
                     user: process.env.EMAIL_USER,
                     pass: process.env.EMAIL_PASS,
